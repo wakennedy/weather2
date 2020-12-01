632cbe0c8e23c35f3a8d86e0ff86955a737bd0e7
wakennedy <alexkenned@gmail.com>
2020-12-01 15:38:42 -0800

    dotenv

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "./App.css";
+const BASEURL = "http://api.weatherapi.com/v1";
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+const LOCATION = "98115";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    currentWeather: [],
+    forecastWeather: {},
+  };
+  async fetchweather() {
+    const response = await fetch(
+      `${BASEURL}/forecast.json?key=${API_KEY}&q=${LOCATION}&days=3`
+    );
+    const weather = await response.json();
+    return weather;
+  }
+  async stateUpdate() {
+    const data = await this.fetchweather();
+    const update = await this.setState({ forecastWeather: data });
+    return update;
+  }
+  async componentDidMount() {
+    await this.stateUpdate();
+  }
+  render() {
+    return (
+      <Fragment>
+        <div>TEST</div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
