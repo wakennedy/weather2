74169f48492495a1d666a24dc91b657887467f85
wakennedy <alexkenned@gmail.com>
2020-12-02 11:00:03 -0800

    table html

--- src/daysContainer.js
+++ src/daysContainer.js
@@ -2,17 +2,34 @@ import React, { Component, Fragment } from "react";
 import Day from "./day.js";
 
 export default class DaysContainer extends Component {
-  renderDays = () => {
+  renderInfo = () => {
     const { forecastInfo } = this.props;
     return forecastInfo.forecast.forecastday.map((day) => <Day info={day} />);
   };
+  renderDays() {
+    const { forecastInfo } = this.props;
+    return forecastInfo.forecast.forecastday.map((info) => (
+      <th>{info.day.date}</th>
+    ));
+  }
   render() {
     return (
       <Fragment>
         <div>Day Container</div>
         <br />
-        <div>{this.renderDays()}</div>
+        <div>{this.renderInfo()}</div>
         <br />
+        <table>
+          <tr>
+            <th>Days</th>
+            {this.renderDays()}
+          </tr>
+          <tr />
+          Temps
+          <th>0</th>
+          <th>1</th>
+          <th>2</th>
+        </table>
       </Fragment>
     );
   }
