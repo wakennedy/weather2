6bce80c2945057196bd4630419352e2612d4ac0a
wakennedy <alexkenned@gmail.com>
2020-12-03 14:39:25 -0800

    Json pretty print

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from "react";
 import "./App.css";
 import DaysContainer from "./daysContainer";
+import JSONPretty from "react-json-pretty";
+import Day from "./day";
 const BASEURL = "http://api.weatherapi.com/v1";
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 const LOCATION = "98115";
@@ -34,13 +36,17 @@ class App extends Component {
       },
       location: {},
     },
+    object: "",
   };
   async fetchweather() {
     const response = await fetch(
       `${BASEURL}/forecast.json?key=${API_KEY}&q=${LOCATION}&days=3`
     );
     const weather = await response.json();
-    return weather;
+    const update = await this.setState({ object: weather }, () => {
+      return weather;
+    });
+    return update;
   }
   async stateUpdate() {
     const data = await this.fetchweather();
@@ -53,10 +59,13 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <div>{this.state.forecastWeather.location.name}</div>
+        {/* <div>{this.state.forecastWeather.location.name}</div> */}
         <br />
-
-        <DaysContainer forecastInfo={this.state.forecastWeather} />
+        {/* <DaysContainer /> */}
+        {/* {console.log(this.state.object)} */}
+        <JSONPretty id="json-pretty" data={this.state.object}></JSONPretty>
+        {/* <div className="a">{this.state.object}</div> */}
+        {/* <DaysContainer forecastInfo={this.state.forecastWeather} /> */}
       </Fragment>
     );
   }
